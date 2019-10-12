9694dc274af2185edef7e3b9a4385a614700914c
Andrea R. Nigro <andrea.nigro@ogilvy.com>
2019-10-12 13:17:52 +0200

    Add "aral ff" command to fix firefox localhost issue

---
+++ src/commands/ff.ts
@@ -0,0 +1,22 @@
+import Command from '../base'
+
+import hostile = require('hostile')
+
+export default class Ff extends Command {
+
+  static description = 'fix firefox issue adding a row in hosts file'
+
+  async run() {
+
+    const {args, flags} = this.parse(Ff)
+
+    const workspace_baseurl = (this.project_env_config() as any)['WORKSPACE_BASE_URL']
+
+    this.log('workspace url:' + workspace_baseurl)
+
+    hostile.set('127.0.0.1', workspace_baseurl)
+
+    this.log('fatto:' + workspace_baseurl)
+
+  }
+}
